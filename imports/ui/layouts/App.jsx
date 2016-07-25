cd38c24ad658aaa5bff2e6659d92c3912591d853
imran_nazir <imran.nazir@yahoo.co.uk>
2016-07-25 21:47:04 +0100

    working code

--- imports/ui/layouts/App.jsx
+++ imports/ui/layouts/App.jsx
@@ -12,7 +12,7 @@ import Menu from "../components/Menu.jsx";
 const App = (props) => (
     <span>
         <Menu />
-        <div className="content" id="panel">
+        <div id="panel">
             <header>
                 <div className="top-bar">
                     <button className="toggle-button">☰</button>
@@ -20,9 +20,11 @@ const App = (props) => (
                 </div>
                 <div className="title">
                     <div className="heading">islamiccharities.org</div>
-                    <div className="slogan">Bringing clarity to the UK ummah</div>
+                    <div className="slogan">Bringing clarity to patrons of UK based Islamic charities</div>
                 </div>  
             </header>
+            <section className="content">
+            </section>
         </div>
     </span>
 )
