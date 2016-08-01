0663b9defdf52f74993bbea2c3da0923cd5a84e8
imran_nazir <imran.nazir@yahoo.co.uk>
2016-08-01 19:50:20 +0100

    improved css and html layout

--- imports/ui/layouts/App.jsx
+++ imports/ui/layouts/App.jsx
@@ -14,23 +14,51 @@ const App = (props) => (
         <Menu />
         <div id="panel">
             <header>
-                <nav id="fixed-header" className="nav">                    
+                <nav id="fixed-header">                    
                         <button className="toggle-button">☰</button>
                         <span>charities4islam.co.uk</span>
                 </nav>
-                <div className="title">
-                    <div className="heading"><h1>charities4islam.co.uk</h1></div>
-                    <p className="slogan">Bringing clarity to the ummah</p>
-                </div>  
+                <article className="hero">
+                    <h1 className="page-title">charities4islam.co.uk</h1>
+                    <p className="page-slogan">Bringing clarity to the ummah</p>
+                </article>                
             </header>
-            <section className="content flex three">
-                <div><span>1</span></div>
-                <div><span>2</span></div>
-                <div><span>3</span></div>
-                <div><span>4</span></div>
-                <div><span>5</span></div>
-                <div><span>6</span></div>
-            </section>
+            <main>
+                <div className="wrapper">
+                    <section className="content flex one three-500">
+                        <div className="full two-third-500">
+                            <article className="card intro">
+                                <p>
+                                    The aim of this site is to bring clarity to the UK 
+                                    ummah by showing visitors how charitable donations are 
+                                    collected and spent at Islamic charities across the country. 
+                                    To find a charoty click 'Search' from the menu. 
+                                    All data is provided by the charities commission.
+                                </p>
+                            </article>
+                        </div>
+                        <div className="full third-500">
+                            <article className="card info">
+                                <header></header>
+                                <footer>
+                                    <h3>Misty Forest</h3>
+                                    <button>Like</button>
+                                </footer>
+                            </article>
+                        </div>
+                        <div className="full third-500">
+                            <article className="card info">
+                                <img src="/web/img/forest.jpg"/>
+                                <footer>
+                                    <h3>Misty Forest</h3>
+                                    <button>Like</button>
+                                </footer>
+                            </article>
+                        </div>
+                    </section>                
+                </div>
+            </main>
+            <footer></footer>
         </div>
     </span>
 )
