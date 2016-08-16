9c262036a9c0584404904b0cefd7ddd78a6edd63
imran_nazir <imran.nazir@yahoo.co.uk>
2016-08-16 23:22:58 +0100

    added aldeed:collection2 & attached schema to charitylist collection

--- imports/api/server/charitylist.js
+++ imports/api/server/charitylist.js
@@ -1,3 +1,32 @@
 // collection holding charity commission data
 // server only, no client side cache needed
+import { SimpleSchema } from 'meteor/aldeed:simple-schema';
+import { attachSchema } from 'meteor/aldeed:collection2';
+
 CharityList = new Mongo.Collection("charitylist");
+
+const CharityList.schema = new SimpleSchema({
+    RegisteredCharityNumber: {
+        type: Number
+    },
+    SubsidiaryNumber: {
+        type: Number
+    },
+    CharityName: {
+        type: String
+    },
+    MainCharityName: {
+        type: String
+    },
+    RegistrationStatus: {
+        type: String
+    },
+    PublicEmailAddress: {
+        type: String
+    },
+    MainPhoneNumber: {
+        type: String
+    }
+});
+
+CharityList.attachSchema(CharityList.schema);
