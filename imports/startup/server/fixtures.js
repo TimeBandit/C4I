461d40dcf3700df8f6d94235a4eb594017ef95f0
imran_nazir <imran.nazir@yahoo.co.uk>
2016-08-29 14:05:21 +0100

    successfully tested charity-commission-api, merging into dev branch

--- imports/startup/server/fixtures.js
+++ imports/startup/server/fixtures.js
@@ -2,7 +2,7 @@
 
 import { Meteor } from 'meteor/meteor';
 import { CharityList } from '../../api/server/charitylist';
-import { charComm } from '../../api/server/charityAPI';
+// import { charComm } from '../../api/server/charityAPI';
 
 Meteor.startup(function() {
     // init the db here
@@ -10,14 +10,18 @@ Meteor.startup(function() {
         console.log('CharityList is empty :)');
 
         let args = { APIKey: '755dfeae-434d-4c90-a', strSearch: 'islam' };
+        
         // run the api funtion and store the result in a collection
-        const ccAPI = 'http://apps.charitycommission.gov.uk/Showcharity/API/SearchCharitiesV1/SearchCharitiesV1.asmx?wsdl';
-        // console.log(
-        //     charComm().GetCharitiesByKeyword(args)
-        // );
-        let charities = charComm(ccAPI).GetCharitiesByKeyword(args);
-        console.log(charities);
-        // run a reactive function to fetch induvidual charity info
-        // calculate the derived data.
+        const ccAPI = require('charity-commission-api');
+
+        ccAPI.GetCharitiesByKeyword(args).then(function(result) {
+
+            console.log(result);
+
+        }).catch(function(err) {
+
+            console.log(`Call to ${err.operationName} failed with error: ${err.err}`);
+
+        });
     }
 });
