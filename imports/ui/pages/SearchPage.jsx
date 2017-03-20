662c6602661d74da58f677a759aec4ecd948d47d
imran_nazir <imran.nazir@yahoo.co.uk>
2017-03-20 13:21:47 +0000

    search working

--- imports/ui/pages/SearchPage.jsx
+++ imports/ui/pages/SearchPage.jsx
@@ -89,16 +89,15 @@ export default class SearchPage extends Component {
 					    styleConfig={styleConfig}
 							components={{
 							  Row: CustomRowComponent
-							}} >
-					    {/*>*/}
+							}}>
 					    <RowDefinition>
-					    					      <ColumnDefinition id="Name" title="Name" />
-					    					      <ColumnDefinition id="Established" title="Established" />
-					    					      <ColumnDefinition id="Incoming" title="Incoming" />
-					    					      <ColumnDefinition id="Employees" title="Employees" />
-					    					      <ColumnDefinition id="Postcode" title="Postcode" />
-					    					    </RowDefinition>
-					    					  </Griddle>
+					      <ColumnDefinition id="Name" title="Name" />
+					      <ColumnDefinition id="Established" title="Established" />
+					      <ColumnDefinition id="Incoming" title="Incoming" />
+					      <ColumnDefinition id="Employees" title="Employees" />
+					      <ColumnDefinition id="Postcode" title="Postcode" />
+					    </RowDefinition>
+					  </Griddle>
 		    	</div>
 	    	</div>
       );
@@ -106,80 +105,4 @@ export default class SearchPage extends Component {
       return (<span></span>)
     }
   }
-}
-/*							<RowDefinition>
-						    <ColumnDefinition id="Name" title="Name" />
-						    <ColumnDefinition id="Established" title="Established" />
-						    <ColumnDefinition id="Incoming" title="Incoming" />
-						    <ColumnDefinition id="Employees" title="Employees" />
-						    <ColumnDefinition id="Postcode" title="Postcode" />
-					    </RowDefinition>
-*/
-
-
-
-// <div className="ui cards">
-// 					  <div className="grey card">
-// 					    <div className="content">
-// 					      <div className="header">AMANAT CHARITY TRUST</div>
-// 					      <div className="meta">Location: Birmingham</div>
-// 					      <div className="description">
-// 					        <div className="ui labels">
-// 									  <a className="ui label">
-// 									    Employees
-// 									    <div className="detail">52</div>
-// 									  </a>
-// 									  <a className="ui label">
-// 									    Incoming
-// 									    <div className="detail">£24.91m</div>
-// 									  </a>
-// 									</div>
-// 					      </div>
-// 					    </div>
-// 					    <div className="extra content">
-// 					    	Established: 07/11/1990
-// 					    </div>
-// 					  </div>
-// 					  <div className="green card">
-// 					    <div className="content">
-// 					      <div className="header">UMMAH HELP</div>
-// 					      <div className="meta">Location: Birmingham</div>
-// 					      <div className="description">
-// 					      	<div className="ui labels">
-// 									  <a className="ui label">
-// 									    Employees
-// 									    <div className="detail">10</div>
-// 									  </a>
-// 									  <a className="ui label">
-// 									    Incoming
-// 									    <div className="detail">£763k</div>
-// 									  </a>
-// 									</div>
-// 					      </div>
-// 					    </div>
-// 					    <div className="extra content">
-// 					    	Established: 04/07/2011
-// 					    </div>
-// 					  </div>
-// 					  <div className="green card">
-// 					    <div className="content">
-// 					      <div className="header">SWANSEA MOSQUE AND ISLAMIC COMMUNITY CENTRE</div>
-// 					      <div className="meta">Location: Swansea</div>
-// 					      <div className="description">
-// 					      	<div className="ui labels">
-// 									  <a className="ui label">
-// 									    Employees
-// 									    <div className="detail">13</div>
-// 									  </a>
-// 									  <a className="ui label">
-// 									    Incoming
-// 									    <div className="detail">£523k</div>
-// 									  </a>
-// 									</div>
-// 					      </div>
-// 					    </div>
-// 					    <div className="extra content">
-// 					    	Established: 06/08/2004
-// 					    </div>
-// 					  </div>
-// 					</div>
+};
